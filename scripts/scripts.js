7286d7989249721d57b0027dc2b317c166b7db9c
kayqueR69 <kayquericharlyson@gmail.com>
2025-11-04 10:19:10 -0300

    add: adicionando o js sem necessidade

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+const scrollButton = document.getElementById('scroll-button');
+
+scrollButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 650,
+        behavior: 'smooth'
+    });
+});
+
+/* desenvolvendo a landing page não vi necessidade de JavaScript então fiz isso ai apenas como exemplo */
